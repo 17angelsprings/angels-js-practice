7376f01a77c70f659e4c1eaab8e400c050ba0f76
bobrien20 <bridgetobrien@my.smccd.edu>
2020-12-22 14:16:39 -0800

    can adjust canvas size. will make adjustable eggs next time

--- egg-art/sketch.js
+++ egg-art/sketch.js
@@ -1,14 +1,29 @@
 function setup() {
-  var cnv = createCanvas(2000, 2000);      
-  cnv.position((windowWidth-width)/2, 40); // sets up canvas size
+  var cnv = createCanvas(2000, 1000);      
+  cnv.position((windowWidth-width)/2, 100); // sets up canvas position
   background(5, 5, 5);                     // colors canvas black
-  button = createButton('Save Image');     // creates Save Image button
+
+  button = createButton('Save Image');      // creates Save Image button
   button.position(19, 10);                 // positions button 
-  button.mousePressed(saveArt);            // saves current image
+  button.mousePressed(saveArt);           // saves current image
+
+  button = createButton('Change Canvas Height');    // creates Change Canvas Height button
+  button.position(150, 10);                        // positions button 
+  button.mousePressed(adjustHeight);              // adjusts canvas height
+
+  slider = createSlider(0, 2000, 1000);      // creates canvas height slider 
+  slider.position(150, 40);
+  slider.style('width', '150px');
 }
 
 function saveArt() {
-  saveCanvas('myArt', 'png')  // downloads image as 'myArt.png'
+  saveCanvas('myArt', 'png');  // downloads image as 'myArt.png'
+}
+
+function adjustHeight() {
+  let h = slider.value();
+  resizeCanvas(2000, h);
+  background(5,5,5);
 }
 
 function draw() {
@@ -19,5 +34,4 @@ function draw() {
     ellipse(mouseX, mouseY, 20, 30); // it's an oval placed down wherever the mouse is
   }
 }
-
-// make canvas more adjustible next time?
+// change size of eggs?
